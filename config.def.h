240b05211b8270548b0d49394b05652cf439a51a
fitzland <f1tzl4nd@gmail.com>
2022-03-19 15:49:59 -0500

    add cpu and ram modules

--- config.def.h
+++ config.def.h
@@ -65,6 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ datetime, "%s",           "%F %T" },
+	{ cpu_perc, "CPU: %s%% |", 	NULL },
+	{ ram_perc, "RAM: %s%% |",	NULL },
 	{ ipv4, "%s",           "enp2s0" },
+	{ datetime, "%s",           "%a, %b %d %R" },
 };
