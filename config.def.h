6d64c1a9e2f88d387cd6c9f06785f9fcdbd699e1
fitzland <f1tzl4nd@gmail.com>
2022-03-19 16:04:50 -0500

    tweak format for modules

--- config.def.h
+++ config.def.h
@@ -65,8 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-	{ cpu_perc, "CPU: %s%% |", 	NULL },
-	{ ram_perc, "RAM: %s%% |",	NULL },
-	{ ipv4, "%s",           "enp2s0" },
-	{ datetime, "%s",           "%a, %b %d %R" },
+	{ cpu_perc, "CPU: %s%% | ", 	NULL },
+	{ ram_perc, "RAM: %s%% | ",	NULL },
+	{ ipv4, "%s | ",           "enp2s0" },
+	{ datetime, "%s",           "%a-%b %d | %R " },
 };
